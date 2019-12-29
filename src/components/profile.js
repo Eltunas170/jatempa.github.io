6e1c81fe45629b1a80b51e5af61f0f4bdb17c202
Jorge Atempa <atempaitm@gmail.com>
2019-12-29 04:38:39 -0800

    Update social network properties

--- src/components/profile.js
+++ src/components/profile.js
@@ -11,7 +11,7 @@ const Profile = ({ aboutMe, profilePicture, twitter, github }) => {
   return (
     <>
       <img src={profilePicture} alt="profile" style={{ padding: `20px 20px`, height: `150px`, width: `150px`, borderRadius: `50%` }} />
-      <span><SocialNetwork name="atempa09" url="https://twitter.com/atempa09" logo={twitterLogo} /> <strong>/</strong> <SocialNetwork name="jatempa" url="https://github.com/jatempa" logo={githubLogo} /></span>
+      <span><SocialNetwork name={twitter} url={`https://twitter.com/${twitter}`} logo={twitterLogo} /> <strong>/</strong> <SocialNetwork name={github} url={`https://github.com/${github}`} logo={githubLogo} /></span>
       <section style={{ marginTop: `10px` }}>
         <h3>Acerca de mí</h3>
         <p>{aboutMe}</p>
--- src/components/socialNetwork.js
+++ src/components/socialNetwork.js
@@ -1,16 +1,13 @@
 import PropTypes from "prop-types"
 import React from "react"
 
-const SocialNetwork = ({name, logo, url}) => {
-
-  return (
-    <span>
-      <img src={logo}  style={{ height: `20px`, width: `20px`, borderRadius: `50%` }} alt={`${name}pic`} />
-      {` `}
-      <strong><a href={url}>{name}</a></strong>
-    </span>
-  )
-}
+const SocialNetwork = ({name, logo, url}) => (
+  <span>
+    <img src={logo}  style={{ height: `20px`, width: `20px`, borderRadius: `50%` }} alt={`${name}pic`} />
+    {` `}
+    <strong><a href={url}>{name}</a></strong>
+  </span>
+)
 
 SocialNetwork.propTypes = {
   name: PropTypes.string,
